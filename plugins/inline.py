22fc4e6a597925507bbc36dce3eea775175d3580
Amani Muhammed <71679579+amanimuhammed@users.noreply.github.com>
2021-09-13 10:28:30 +0530

    This is not risky 😌

--- plugins/inline.py
+++ plugins/inline.py
@@ -91,7 +91,7 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('🖥️ How To Own 🖥️', url=f'{TUTORIAL}')
+            InlineKeyboardButton('🔰 Updates Channel 🔰', url='https://t.me/NEWMOVIESALWAYSHD')
         ],
         [
             InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat=query)
