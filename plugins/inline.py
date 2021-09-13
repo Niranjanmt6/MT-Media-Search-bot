cfc9ae6caa05e1daddcb14c5c128ab211035a67e
Niranjanmt6 <88812743+Niranjanmt6@users.noreply.github.com>
2021-09-13 08:24:47 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -91,7 +91,7 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('🖥️ How To Own 🖥️', url=f'{TUTORIAL}')
+            InlineKeyboardButton('🔰 JOIN CHANNEL 🔰', url='https://t.me/newrelasemalayalammovies')
         ],
         [
             InlineKeyboardButton('🔍 Search again 🔎', switch_inline_query_current_chat=query)
